fbe1af08e7be335318f0ad792b773233ce0f1d48
Matthew <zorosay@gmail.com>
2020-08-06 22:52:19 +0800

    Update UserDao.java
    用户Dao接口添加新方法

--- src/com/h/dao/UserDao.java
+++ src/com/h/dao/UserDao.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface UserDao {
 
     List<User> queryUsers() throws SQLException;
+
+    User queryUserById(String userId) throws SQLException;
+
+    void addUser(User user) throws SQLException;
 }
