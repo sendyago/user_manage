abe4ebd63a0182f985b7e05b03957f081d745456
Matthew <zorosay@gmail.com>
2020-08-06 22:52:03 +0800

    Update MysqlDB.java
    修改连接mysql时使用指定编码

--- src/com/h/db/MysqlDB.java
+++ src/com/h/db/MysqlDB.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class MysqlDB {
 
     private final static String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/userdb";
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/userdb?useUnicode=true&characterEncoding=utf8";
     private final static String DB_USER = "root";
     private final static String DB_PASSWD = "123456";
 
