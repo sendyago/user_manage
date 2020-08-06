7b4ab100c6aee56e4e8ee428aa7f21c39f7aa53e
Matthew <zorosay@gmail.com>
2020-08-06 22:52:47 +0800

    Update UserDaoImpl.java
    实现用户Dao接口，根据用户id查询用户信息，添加用户信息到数据库

--- src/com/h/dao/impl/UserDaoImpl.java
+++ src/com/h/dao/impl/UserDaoImpl.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
+
     @Override
     public List<User> queryUsers() throws SQLException {
         String sql = "select u.*, r.roleName from userInfo u left join roleInfo r on r.roleId = u.roleId";
@@ -41,5 +42,58 @@ public class UserDaoImpl implements UserDao {
             conn.close();
         }
         return list;
+    }
+
+    @Override
+    public User queryUserById(String userId) throws SQLException {
+        String sql = "select u.* from userInfo u where u.userId='"+userId+"'";
+        User user = null;
+        MysqlDB mysqlDB = new MysqlDB();
+        Connection conn = mysqlDB.getConn();
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                user = new User();
+                user.setUserId(rs.getString("userId"));
+                user.setUserName(rs.getString("userName"));
+                user.setGender(rs.getInt("gender"));
+                user.setUserPassword(rs.getString("userPassword"));
+                user.setRoleId(rs.getInt("roleId"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        return user;
+    }
+
+    @Override
+    public void addUser(User user) throws SQLException {
+        StringBuffer sql = new StringBuffer("insert into userInfo(userId,userName,userPassword,gender,roleId) values(");
+        sql.append("'" + user.getUserId() + "',");
+        sql.append("'" + user.getUserName() + "',");
+        sql.append("'" + user.getUserPassword() + "',");
+        sql.append( user.getGender() + ",");
+        sql.append( user.getRoleId() );
+        sql.append(")");
+        MysqlDB mysqlDB = new MysqlDB();
+        Connection conn = mysqlDB.getConn();
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(sql.toString());
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            stmt.close();
+            conn.close();
+        }
     }
 }
