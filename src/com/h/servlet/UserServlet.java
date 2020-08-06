8ac99d007eae0d501cd3c4838795ec5e1ea6d5f0
Matthew <zorosay@gmail.com>
2020-08-06 22:49:29 +0800

    Update UserServlet.java
    新增用户添加功能，以及将不同功能通过switch分类

--- src/com/h/servlet/UserServlet.java
+++ src/com/h/servlet/UserServlet.java
@@ -16,12 +16,52 @@ import java.util.List;
 public class UserServlet extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         UserService userService = new UserServiceImpl();
+        String type = req.getParameter("type");
         try {
-            List<User> list = userService.queryUsers();
-            req.setAttribute("userList", list);
-            req.getRequestDispatcher("/pages/user/userList.jsp").forward(req, resp);
+            switch (type) {
+                case "1": // 保存
+                    User user = new User();
+                    user.setUserId(req.getParameter("userId"));
+                    user.setUserName(req.getParameter("userName"));
+                    user.setUserPassword(req.getParameter("userPassword"));
+                    String genderStr = req.getParameter("gender");
+                    if (genderStr != null) {
+                        user.setGender(Integer.valueOf(genderStr));
+                    }
+                    String roleIdStr = req.getParameter("roleId");
+                    if (roleIdStr == null) {
+                        user.setRoleId(2);
+                    } else {
+                        user.setRoleId(Integer.valueOf(roleIdStr));
+                    }
+                    userService.addUser(user);
+                    req.getRequestDispatcher("/userServlet?type=0").forward(req, resp);
+                    break;
+                case "2": // 编辑
+                    break;
+                case "3": // 删除
+                    break;
+                case "4": // 验证
+                    String userId = req.getParameter("userId");
+                    User user1 = userService.queryUserById(userId);
+                    if (user1 == null) {
+                        resp.getWriter().print("1");
+                    } else {
+                        resp.getWriter().print("2");
+                    }
+                    break;
+                default: // 查询
+                    List<User> list = userService.queryUsers();
+                    req.setAttribute("userList", list);
+                    req.getRequestDispatcher("/pages/user/userList.jsp").forward(req, resp);
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
