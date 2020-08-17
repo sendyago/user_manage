ad9ef0384380721b10357dc564017c88984fc3da
Matthew <zorosay@gmail.com>
2020-08-17 16:31:04 +0800

    用户Service更新：增加用户分页查询，用户编辑与删除

--- src/com/h/service/impl/UserServiceImpl.java
+++ src/com/h/service/impl/UserServiceImpl.java
@@ -10,21 +10,37 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+    UserDao userDao = new UserDaoImpl();
+
     @Override
-    public List<User> queryUsers() throws SQLException {
-        UserDao userDao = new UserDaoImpl();
-        return userDao.queryUsers();
+    public List<User> queryUsers(User user, int pageNum, int lineNum) throws SQLException {
+        int limit_x = (pageNum - 1) * lineNum;
+        int limit_y = lineNum;
+        return userDao.queryUsers(user, limit_x, limit_y);
+    }
+
+    @Override
+    public int queryUserCount(User u) throws SQLException {
+        return userDao.queryUserCount(u);
     }
 
     @Override
     public User queryUserById(String userId) throws SQLException {
-        UserDao userDao = new UserDaoImpl();
         return userDao.queryUserById(userId);
     }
 
     @Override
     public void addUser(User user) throws SQLException {
-        UserDao userDao = new UserDaoImpl();
         userDao.addUser(user);
+    }
+
+    @Override
+    public void editUser(User user) throws SQLException {
+        userDao.editUser(user);
+    }
+
+    @Override
+    public void deleteUser(String userId) throws SQLException {
+        userDao.deleteUser(userId);
     }
 }
