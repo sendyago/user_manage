8b28e1bb95cc97de39de176f9da025cd570fb07e
Matthew <zorosay@gmail.com>
2020-08-24 11:06:18 +0800

    新建Role实体类

---
+++ src/com/h/vo/Role.java
@@ -0,0 +1,24 @@
+package com.h.vo;
+
+public class Role {
+
+    private Integer roleId;
+    private String roleName;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+}
